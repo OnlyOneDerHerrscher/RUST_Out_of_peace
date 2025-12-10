b9e20571aebe20b564d5e3f51be70f830514f22d
OnlyOneDerHerrscher <antipeklo@gmail.com>
2025-12-10 07:59:21 -0600

    build: add function as plk_1

--- roop/src/main.rs
+++ roop/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
 //variables for plk
-//
-  
+    println!("SSH");
+    plk_1();
+    }
+
+fn plk_1() {
+
+
     let plk_1_1 = ["BPS1.128 - basic controller [1]", "BPS1.C1/1E - communication card for Ethernet [1]", "PVA3.02/V 20 - programming card [1]", "PTM1.4R1K - Module measuring for LG-sensors [3]", "PTM1.2U10 - measuring module 0-10V [4]", "PTM1.4D20 - discrete alarming module [9]", "PTM1.2Q250 - relay switch module ~250, 4A [11]", "PTX1.6 [2]", "PTX1.01 - power module 24V and P-Bus [2]", "PTG1.32 (modules 1..32) [1]", "PTG1.64 (modules 33..64) [1]", "PTG96 (modules 65..96)", "PDG1.128 (modules 97..128) [1]" ];
+
+    let plk_1_2 = ["Temperature immersion sensor Siemens QAE26.91 [9]"];
+
+    let plk_1_3 = ["Water pressure sensor with analogue output 0-10 V (range of measuring 0 - 1 MPa) QBE2000 -p10 [2]", "pressure sensor QBE620-P [2]"];
 
     let plk_1_2 = ["Temperature immersion sensor Siemens QAE26.91 [9]"];
 
@@ -25,7 +34,6 @@ fn main() {
 
     println!("SH,H!");
     println!("{:?}", plk_1_1);
-
     println!("{:?}", plk_1_2);
     println!("{:?}", plk_1_3);
     println!("{:?}", plk_1_4);
@@ -35,4 +43,6 @@ fn main() {
     println!("{:?}", plk_1_8);
     println!("{:?}", plk_1_9);
     println!("{:?}", plk_1_10);
+  
+
 }
---
+++ roop/target/debug/deps/roop-5217cce46a559cee.d
@@ -0,0 +1,5 @@
+/home/wind/workspace/pashyr/RUST_Out_of_peace/roop/target/debug/deps/roop-5217cce46a559cee.d: src/main.rs
+
+/home/wind/workspace/pashyr/RUST_Out_of_peace/roop/target/debug/deps/roop-5217cce46a559cee: src/main.rs
+
+src/main.rs:
