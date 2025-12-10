26fcb1db30f9dbba8001fbe5d789382c4dc15e21
OnlyOneDerHerrscher <antipeklo@gmail.com>
2025-12-10 07:32:25 -0600

    feat: add some plcs as vectors

--- roop/src/main.rs
+++ roop/src/main.rs
@@ -2,8 +2,37 @@ fn main() {
 //variables for plk
 //
   
-    let plk_1 = ["BPS1.128 - basic controller [1]", "BPS1.C1/1E - communication card for Ethernet [1]", "PVA3.02/V 20 - programming card [1]", "PTM1.4R1K - Module measuring for LG-sensors [3]", "PTM1.2U10 - measuring module 0-10V [4]", "PTM1.4D20 - discrete alarming module [9]", "PTM1.2Q250 - relay switch module ~250, 4A [11]", "PTX1.6 [2]", "PTX1.01 - power module 24V and P-Bus [2]" ];
-    println!("SH,H!");
-    println!("{:?}", plk_1);
+    let plk_1_1 = ["BPS1.128 - basic controller [1]", "BPS1.C1/1E - communication card for Ethernet [1]", "PVA3.02/V 20 - programming card [1]", "PTM1.4R1K - Module measuring for LG-sensors [3]", "PTM1.2U10 - measuring module 0-10V [4]", "PTM1.4D20 - discrete alarming module [9]", "PTM1.2Q250 - relay switch module ~250, 4A [11]", "PTX1.6 [2]", "PTX1.01 - power module 24V and P-Bus [2]", "PTG1.32 (modules 1..32) [1]", "PTG1.64 (modules 33..64) [1]", "PTG96 (modules 65..96)", "PDG1.128 (modules 97..128) [1]" ];
 
+    let plk_1_2 = ["Temperature immersion sensor Siemens QAE26.91 [9]"];
+
+    let plk_1_3 = ["Water pressure sensor with analogue output 0-10 V (range of measuring 0 - 1 MPa) QBE2000 -p10 [2]", "pressure sensor QBE620-P [2]"];
+    
+    let plk_1_4 = ["Water pressure sensor with analogue output 0-10 V (range of measuring 0 - 1 MPa for pipe mounting)"];
+
+    let plk_1_5 = ["Differential pressure relay ДЕМ- 202 on pumps (0..0.2 MPA) [1]"];
+
+    let plk_1_6 = ["Humidity and temperature sensor QFA2040 in the server room otm. 15.600"];
+
+    let plk_1_7 = ["ДМ- 2010сг - electric contact monometr on pumps [1]"];
+
+    let plk_1_8 = ["ДМ- 2010сг - electric contact monometr on pumps [1]"];
+     
+
+    let plk_1_9 = ["QAC22 - Temperature sensor of outside air [1]"];
+
+    let plk_1_10 = ["Unstandart oborydovanie PLK-1"];
+
+    println!("SH,H!");
+    println!("{:?}", plk_1_1);
+
+    println!("{:?}", plk_1_2);
+    println!("{:?}", plk_1_3);
+    println!("{:?}", plk_1_4);
+    println!("{:?}", plk_1_5);
+    println!("{:?}", plk_1_6);
+    println!("{:?}", plk_1_7);
+    println!("{:?}", plk_1_8);
+    println!("{:?}", plk_1_9);
+    println!("{:?}", plk_1_10);
 }
