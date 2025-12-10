b53be90a7274a242ac6a4cc4f2ab13397273f4c2
OnlyOneDerHerrscher <antipeklo@gmail.com>
2025-12-10 02:34:20 -0600

    feat: add vector and some shit

--- roop/src/main.rs
+++ roop/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+//variables for plk
+//
+  
+    let plk_1 = ["BPS1.128 - basic controller [1]", "BPS1.C1/1E - communication card for Ethernet [1]", "PVA3.02/V 20 - programming card [1]", "PTM1.4R1K - Module measuring for LG-sensors [3]", "PTM1.2U10 - measuring module 0-10V [4]", "PTM1.4D20 - discrete alarming module [9]", "PTM1.2Q250 - relay switch module ~250, 4A [11]", "PTX1.6 [2]", "PTX1.01 - power module 24V and P-Bus [2]" ];
+    println!("SH,H!");
+    println!("{:?}", plk_1);
+
 }
